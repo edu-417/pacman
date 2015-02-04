72b38a82cbc443c86ab7f9a0993c0baf203ff263
Eduardo <edgame127@gmail.com>
2015-02-04 16:59:31 -0500

    feat: added main js script and index html

---
+++ js/Main.js
@@ -0,0 +1,18 @@
+﻿; (function () {
+    window.onload = function () {
+        var myGame = new Game();
+
+        function main(timestamp) {
+            var dt = Math.floor(timestamp - myGame.previousTimeStamp);            
+            myGame.previousTimeStamp = timestamp;
+            myGame.update(dt);
+            myGame.draw();               
+            myGame.stopMain = window.requestAnimationFrame(main);                     
+        }
+
+        myGame.previousTimeStamp = performance.now();
+        myGame.setInitialState();
+        main(performance.now());
+
+    };
+})();
